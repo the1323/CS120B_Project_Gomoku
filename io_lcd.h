890ad454cafcb3c192b2ee4035ea703f2a85728f
the1323 <48039916+the1323@users.noreply.github.com>
2021-11-21 20:16:30 -0800

    Add files via upload

--- io_lcd.h
+++ io_lcd.h
@@ -1,44 +1,38 @@
 #ifndef IO_LCD_H
 #define IO_LCD_H
 
+#include<stdio.h>
+#include<stdbool.h>
+#include<inttypes.h>
 #include<avr/io.h>
 #include<util/delay.h>
 #include<avr/pgmspace.h>
 #include<limits.h>
-#include<inttypes.h>
 
-/* Author: The033, CS120B UCR final project.
- * This driver library is designed for ILI9341 Res 280x320 TFT LCD for Atmega1284/p.
- * This display has same interface as 5110 for all hardware pin arragnement
- * and operating voltage.
- *
- * There are 8 output pins required for display.
- *
- * Here are the pinout
- * ILI9341   Atmega1284/p
- * VCC       3.3V(+)
- * GND       GND(-)
- * CS        PB4
- * RESET     PB0
- * D/C       PB1
- * MOSI      PB5
- * SCK       PB7
- * LED       3.3V(+)
- *
 
-*/
+#define LCD_PORT PORTB
+#define LCD_DDR DDRB
+#define LCD_PIN PINB
+#define dc 1
+#define cs 4
+#define rst 0
 
-/*
- * typedef according to POSIX:
- * uint8_t  -> unsigned int 8 bits
- * uint16_t -> unsigned int 16 bits
- */
-void LCD_setup(void);
+
+
+#define BLACK 0x0000
+#define WHITE 0xFFFF
+
+
+void LCD_Setup(void);
 void LCD_WriteCommand(uint8_t com);
-void LCD_WriteData(uint8_t data);
+void LCD_WriteData8(uint8_t data);
+void LCD_WriteData16(uint16_t data);
 void LCD_init(void);
-void LCD_pushcolour(uint16_t colour);
-void LCD_ClearScreen(uint16_t colour);
-//void LCD_drawpixel(uint16_t x3,uint16_t y3,uint16_t colour1);
-void delay_ms(int miliSec);
+void LCD_ClearScreen();
+void LCD_DrawPixel(uint16_t x3,uint16_t y3,uint16_t colour1);
+void LCD_DrawBlock(uint16_t x,uint16_t y,uint16_t w,uint16_t h,uint16_t colour);
+void LCD_WriteChar( unsigned char c);
+void LCD_WriteString(unsigned char* c);
+void LCD_Cursor(uint16_t x,uint16_t y);
+
 #endif // IO_LCD_H
